580dfb3c0275616a5dd82a17136e417f4bcdf070
Daniel Castellote <daniel.castellote.b@gmail.com>
2022-01-20 20:02:28 +0100

    Creacion de clases y persistencia

---
+++ src/main/java/repository/CrudRepository.java
@@ -0,0 +1,21 @@
+package repository;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CrudRepository<T, ID> {
+
+    // Operaciones CRUD
+
+    List<T> findAll() throws SQLException;
+
+    T getById(ID id) throws SQLException;
+
+    T save(T t) throws SQLException;
+
+    T update(T t) throws SQLException;
+
+    T delete(T t) throws SQLException;
+
+
+}
---
+++ src/main/java/service/BaseService.java
@@ -0,0 +1,33 @@
+package service;
+import repository.CrudRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RequiredArgsConstructor // Requerimos un constructor con al menos las propiedades finales
+public abstract class BaseService<T, ID, R extends CrudRepository<T, ID>> {
+    protected final R repository;
+
+    // Operaciones CRUD
+
+    public List<T> findAll() throws SQLException {
+        return repository.findAll();
+    }
+
+    public T getById(ID id) throws SQLException {
+        return repository.getById(id);
+    }
+
+    public T save(T t) throws SQLException {
+        return repository.save(t);
+    }
+
+    public T update(T t) throws SQLException {
+        return repository.update(t);
+    }
+
+    public T delete(T t) throws SQLException {
+        return repository.delete(t);
+    }
+}
