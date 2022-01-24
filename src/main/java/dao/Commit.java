68ff97d8757d94125af17e23772497901cea3b6a
Daniel Castellote <daniel.castellote.b@gmail.com>
2022-01-24 19:37:54 +0100

    Cambios commit y Repositorio dao

--- src/main/java/dao/Commit.java
+++ src/main/java/dao/Commit.java
@@ -1,4 +1,87 @@
 package dao;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "commit")
+@NamedQuery(name = "Commit.findAll", query = "SELECT c FROM Commit c")
 public class Commit {
+
+    private long id;
+    private String titulo;
+    private String texto;
+    private Date fCreacion;
+    private Programador autor;
+    private Repositorio repositorio;
+
+    public Commit(String titulo, String texto, Date fCreacion, Programador autor, Repositorio repositorio) {
+        this.titulo = titulo;
+        this.texto = texto;
+        this.fCreacion = fCreacion;
+        this.autor = autor;
+        this.repositorio = repositorio;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    public long getId() {
+        return id;
+    }
+    @Basic
+    @Column(name = "titulo", nullable = false, length = 100)
+    public String getTitulo() {
+        return titulo;
+    }
+    @Basic
+    @Column(name = "texto", nullable = false, length = 100)
+    public String getTexto() {
+        return texto;
+    }
+    @Basic
+    @Column(name = "fCreacion", nullable = false, length = 100)
+    public Date getfCreacion() {
+        return fCreacion;
+    }
+    @ManyToOne
+    @JoinColumn(name = "programador_id", referencedColumnName = "id", nullable = false)
+    public Programador getAutor() {
+        return autor;
+    }
+    @ManyToOne
+    @JoinColumn(name = "repositorio_id", referencedColumnName = "id", nullable = false)
+    public Repositorio getRepositorio() {
+        return repositorio;
+    }
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public void setfCreacion(Date fCreacion) {
+        this.fCreacion = fCreacion;
+    }
+
+    public void setAutor(Programador autor) {
+        this.autor = autor;
+    }
+
+    public void setRepositorio(Repositorio repositorio) {
+        this.repositorio = repositorio;
+    }
 }
--- src/main/java/dao/Repositorio.java
+++ src/main/java/dao/Repositorio.java
@@ -3,14 +3,12 @@ package dao;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "repositorio")
 @NamedQuery(name = "Repositorio.findAll", query = "SELECT c FROM Repositorio c")
@@ -21,9 +19,56 @@ public class Repositorio {
     private Set<Issue> issues;
     private Set<Commit> commits;
 
-    public Repositorio(Date fCreacion, Set<Issue> issues, Set<Commit> commits) {
+    public Repositorio() {
+        this.fCreacion = new Date(System.currentTimeMillis());
+        this.issues = new HashSet<>();
+        this.commits = new HashSet<>();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    public long getId() {
+        return id;
+    }
+
+    @Basic
+    @Column(name = "nombre", nullable = false, length = 100)
+    public Date getfCreacion() {
+        return fCreacion;
+    }
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "repositorio", orphanRemoval = true, cascade = CascadeType.REMOVE)
+    public Set<Issue> getIssues() {
+        return issues;
+    }
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "repositorio", orphanRemoval = true, cascade = CascadeType.REMOVE)
+    public Set<Commit> getCommits() {
+        return commits;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setfCreacion(Date fCreacion) {
         this.fCreacion = fCreacion;
+    }
+
+    public void setIssues(Set<Issue> issues) {
         this.issues = issues;
+    }
+
+    public void setCommits(Set<Commit> commits) {
         this.commits = commits;
+    }
+
+    @Override
+    public String toString() {
+        return "Repositorio{" +
+                "id=" + id +
+                ", fCreacion=" + fCreacion +
+                '}';
     }
 }
