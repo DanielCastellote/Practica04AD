8604d827913fa133a62cf2ef850aaa3e83602f0d
Daniel Castellote <daniel.castellote.b@gmail.com>
2022-01-24 19:11:12 +0100

    Cambios departamento

--- src/main/java/controller/DepartamentoController.java
+++ src/main/java/controller/DepartamentoController.java
@@ -1,4 +1,70 @@
 package controller;
 
+import dto.DepartamentoDTO;
+import repository.DepartamentoRepository;
+import service.DepartamentoService;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class DepartamentoController {
+    private static DepartamentoController controller = null;
+
+    private final DepartamentoService departamentoService;
+
+    private DepartamentoController(DepartamentoService programadorService) {
+        this.departamentoService = programadorService;
+    }
+
+    public static DepartamentoController getInstance() {
+        if (controller == null) {
+            controller = new DepartamentoController(new DepartamentoService(new DepartamentoRepository()));
+        }
+        return controller;
+    }
+
+    public List<DepartamentoDTO> getAllDepartamento() {
+        try {
+            return departamentoService.getAllDepartamento();
+        } catch (SQLException e) {
+            System.err.println("Error DepartamentoController en getAllDepartamento: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public DepartamentoDTO getDepartamentoById(Long id) {
+        try {
+            return departamentoService.getDepartamentoById(id);
+        } catch (SQLException e) {
+            System.err.println("Error DepartamentoController en getDepartamentoById " + e.getMessage());
+            return null;
+        }
+    }
+
+    public DepartamentoDTO postDepartamento(DepartamentoDTO departamentoDTO) {
+        try {
+            return departamentoService.postDepartamento(departamentoDTO);
+        } catch (SQLException e) {
+            System.err.println("Error DepartamentoController en postDepartamento " + e.getMessage());
+            return null;
+        }
+    }
+
+    public DepartamentoDTO updateDepartamento(DepartamentoDTO departamentoDTO) {
+        try {
+            return departamentoService.updateDepartamento(departamentoDTO);
+        } catch (SQLException e) {
+            System.err.println("Error DepartamentoController en updateDepartamento " + e.getMessage());
+            return null;
+        }
+    }
+
+    public DepartamentoDTO deleteDepartamento(DepartamentoDTO departamentoDTO) {
+        try {
+            return departamentoService.deleteDepartamento(departamentoDTO);
+        } catch (SQLException e) {
+            System.err.println("Error DepartamentoController en deleteDepartamento " + e.getMessage());
+            return null;
+        }
+    }
 }
--- src/main/java/service/DepartamentoService.java
+++ src/main/java/service/DepartamentoService.java
@@ -1,4 +1,49 @@
 package service;
 
-public class DepartamentoService {
+import dao.Departamento;
+import dao.Programador;
+import dto.DepartamentoDTO;
+import dto.ProgramadorDTO;
+import mapper.DepartamentoMapper;
+import mapper.ProgramadorMapper;
+import repository.DepartamentoRepository;
+import repository.ProgramadorRepository;
+import utils.Cifrador;
+
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+
+public class DepartamentoService extends BaseService<Departamento, Long, DepartamentoRepository> {
+
+    DepartamentoMapper mapper = new DepartamentoMapper();
+
+
+    public DepartamentoService(DepartamentoRepository repository) {
+        super(repository);
+    }
+
+    public List<DepartamentoDTO> getAllDepartamento() throws SQLException {
+        return mapper.toDTO(this.findAll());
+    }
+
+    public DepartamentoDTO getDepartamentoById(Long id) throws SQLException {
+        return mapper.toDTO(this.getById(id));
+    }
+
+    public DepartamentoDTO postDepartamento(DepartamentoDTO departamentoDTO) throws SQLException {
+
+        Departamento dept = this.save(mapper.fromDTO(departamentoDTO));
+        return mapper.toDTO(dept);
+    }
+
+    public DepartamentoDTO updateDepartamento(DepartamentoDTO departamentoDTO) throws SQLException {
+        Departamento dept = this.update(mapper.fromDTO(departamentoDTO));
+        return mapper.toDTO(dept);
+    }
+
+    public DepartamentoDTO deleteDepartamento(DepartamentoDTO departamentoDTO) throws SQLException {
+        Departamento dept = this.delete(mapper.fromDTO(departamentoDTO));
+        return mapper.toDTO(dept);
+    }
 }
