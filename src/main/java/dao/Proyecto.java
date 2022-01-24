93add6c41de710b405a72112d98678d33854ead6
javiergmun <javiergmun@gmail.com>
2022-01-24 19:26:04 +0100

    proyecto dao

--- src/main/java/dao/Proyecto.java
+++ src/main/java/dao/Proyecto.java
@@ -1,4 +1,85 @@
 package dao;
 
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "proyecto")
+@NamedQuery(name = "Proyecto.findAll", query = "SELECT c FROM Proyecto c")
 public class Proyecto {
+    private long id;
+    private String nombre;
+    private double presupuesto;
+    private Date fInicio;
+    private Date fFin;
+
+    public Proyecto(String nombre, double presupuesto, Date fInicio, Date fFin) {
+        this.nombre = nombre;
+        this.presupuesto = presupuesto;
+        this.fInicio = fInicio;
+        this.fFin = fFin;
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name="id",nullable = false)
+    public long getId(){
+        return id;
+    }
+    @Basic
+    @Column(name = "nombre", nullable = false, length = 100)
+    public String getNombre() {
+        return nombre;
+    }
+    @Basic
+    @Column(name = "presupuesto", nullable = false, length = 100)
+    public double getPresupuesto() {
+        return presupuesto;
+    }
+    @Basic
+    @Column(name = "fechaInicio", nullable = false, length = 100)
+    public Date getfInicio() {
+        return fInicio;
+    }
+    @Basic
+    @Column(name = "fechaFin", nullable = false, length = 100)
+    public Date getfFin() {
+        return fFin;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPresupuesto(double presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+
+    public void setfInicio(Date fInicio) {
+        this.fInicio = fInicio;
+    }
+
+    public void setfFin(Date fFin) {
+        this.fFin = fFin;
+    }
+
+    @Override
+    public String toString() {
+        return "Proyecto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", presupuesto=" + presupuesto +
+                ", fInicio=" + fInicio +
+                ", fFin=" + fFin +
+                '}';
+    }
 }
