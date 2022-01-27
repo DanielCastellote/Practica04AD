8c9ff00da99a597f331edeafaecc79ff55fd1927
Daniel Castellote <daniel.castellote.b@gmail.com>
2022-01-27 10:35:19 +0100

    Login done

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,4 +1,72 @@
 package controller;
 
+import dto.LoginDTO;
+import repository.LoginRepository;
+import service.LoginService;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class LoginController {
+
+    private static LoginController controller = null;
+
+    private final LoginService loginService;
+
+
+    private LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    public static LoginController getInstance() {
+        if (controller == null) {
+            controller = new LoginController(new LoginService(new LoginRepository()));
+        }
+        return controller;
+    }
+
+    public List<LoginDTO> getAllLogin() {
+        try {
+            return loginService.getAllLogin();
+        } catch (SQLException e) {
+            System.err.println("Error LoginController en getAllLogin: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public LoginDTO getLoginById(Long id) {
+        try {
+            return loginService.getLoginById(id);
+        } catch (SQLException e) {
+            System.err.println("Error LoginController en getLoginById " + e.getMessage());
+            return null;
+        }
+    }
+
+    public LoginDTO postLogin(LoginDTO loginDTO) {
+        try {
+            return loginService.postLogin(loginDTO);
+        } catch (SQLException e) {
+            System.err.println("Error LoginController en postLogin " + e.getMessage());
+            return null;
+        }
+    }
+
+    public LoginDTO updateLogin(LoginDTO loginDTO) {
+        try {
+            return loginService.updateLogin(loginDTO);
+        } catch (SQLException e) {
+            System.err.println("Error LoginController en updateLogin " + e.getMessage());
+            return null;
+        }
+    }
+
+    public LoginDTO deleteLogin(LoginDTO loginDTO) {
+        try {
+            return loginService.deleteLogin(loginDTO);
+        } catch (SQLException e) {
+            System.err.println("Error LoginController en deleteLogin " + e.getMessage());
+            return null;
+        }
+    }
 }
--- src/main/java/service/LoginService.java
+++ src/main/java/service/LoginService.java
@@ -1,4 +1,47 @@
 package service;
 
-public class LoginService {
+import dao.Issue;
+import dao.Login;
+import dto.IssueDTO;
+import dto.LoginDTO;
+import mapper.IssueMapper;
+import mapper.LoginMapper;
+import repository.IssueRepository;
+import repository.LoginRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class LoginService extends BaseService<Login, Long, LoginRepository>{
+
+    LoginMapper mapper = new LoginMapper();
+
+
+    public LoginService(LoginRepository repository) {
+        super(repository);
+    }
+
+    public List<LoginDTO> getAllLogin() throws SQLException {
+        return mapper.toDTO(this.findAll());
+    }
+
+    public LoginDTO getLoginById(Long id) throws SQLException {
+        return mapper.toDTO(this.getById(id));
+    }
+
+    public LoginDTO postLogin(LoginDTO loginDTO) throws SQLException {
+
+        Login issue = this.save(mapper.fromDTO(loginDTO));
+        return mapper.toDTO(issue);
+    }
+
+    public LoginDTO updateLogin(LoginDTO loginDTO) throws SQLException {
+        Login issue = this.update(mapper.fromDTO(loginDTO));
+        return mapper.toDTO(issue);
+    }
+
+    public LoginDTO deleteLogin(LoginDTO loginDTO) throws SQLException {
+        Login issue = this.delete(mapper.fromDTO(loginDTO));
+        return mapper.toDTO(issue);
+    }
 }
