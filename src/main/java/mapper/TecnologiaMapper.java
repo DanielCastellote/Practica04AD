1307053a67d18ab2ee38fe08cade96c1f3d2db77
javiergmun <javiergmun@gmail.com>
2022-01-28 09:41:52 +0100

    Mappers update

--- src/main/java/mapper/TecnologiaMapper.java
+++ src/main/java/mapper/TecnologiaMapper.java
@@ -1,4 +1,28 @@
 package mapper;
 
-public class TecnologiaMapper {
+import dao.Commit;
+import dao.Tecnologia;
+import dto.CommitDTO;
+import dto.TecnologiaDTO;
+
+public class TecnologiaMapper extends BaseMapper<Tecnologia, TecnologiaDTO> {
+    @Override
+    public Tecnologia fromDTO(TecnologiaDTO item) {
+        Tecnologia tecnologia = new Tecnologia();
+        if (item.getId() != null) {
+            tecnologia.setId(item.getId());
+        }
+        tecnologia.setNombre(item.getNombre());
+        tecnologia.setProgramador(item.getProgramador());
+        return tecnologia;
+    }
+
+    @Override
+    public TecnologiaDTO toDTO(Tecnologia item) {
+        return TecnologiaDTO.builder()
+                .id(item.getId())
+                .nombre(item.getNombre())
+                .programador(item.getProgramador())
+                .build();
+    }
 }
