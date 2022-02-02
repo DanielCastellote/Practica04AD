6b3f5702eff6f7c7072ccabe1cdf9c0da8677a10
javiergmun <javiergmun@gmail.com>
2022-02-02 18:47:33 +0100

    Tecnologia Controller creation

--- src/main/java/controller/TecnologiaController.java
+++ src/main/java/controller/TecnologiaController.java
@@ -1,4 +1,75 @@
 package controller;
 
+import dto.LoginDTO;
+import dto.TecnologiaDTO;
+import repository.LoginRepository;
+import repository.TecnologiaRepository;
+import service.LoginService;
+import service.TecnologiaService;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class TecnologiaController {
+
+    private static TecnologiaController controller = null;
+
+    private final TecnologiaService tecnologiaService;
+
+
+    private TecnologiaController(TecnologiaService tecnologiaService) {
+        this.tecnologiaService = tecnologiaService;
+    }
+
+    public static TecnologiaController getInstance() {
+        if (controller == null) {
+            controller = new TecnologiaController(new TecnologiaService(new TecnologiaRepository()));
+        }
+        return controller;
+    }
+
+    public List<TecnologiaDTO> getAllTecnologias() {
+        try {
+            return tecnologiaService.getAllTecnologias();
+        } catch (SQLException e) {
+            System.err.println("Error LoginController en getAllLogin: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public TecnologiaDTO getTecnologiaById(Long id) {
+        try {
+            return tecnologiaService.getTecnologiaById(id);
+        } catch (SQLException e) {
+            System.err.println("Error LoginController en getLoginById " + e.getMessage());
+            return null;
+        }
+    }
+
+    public TecnologiaDTO postTecnologia(TecnologiaDTO tecnologiaDTO) {
+        try {
+            return tecnologiaService.postTecnologia(tecnologiaDTO);
+        } catch (SQLException e) {
+            System.err.println("Error LoginController en postLogin " + e.getMessage());
+            return null;
+        }
+    }
+
+    public TecnologiaDTO updateTecnologia(TecnologiaDTO tecnologiaDTO) {
+        try {
+            return tecnologiaService.updateTecnologia(tecnologiaDTO);
+        } catch (SQLException e) {
+            System.err.println("Error LoginController en updateLogin " + e.getMessage());
+            return null;
+        }
+    }
+
+    public TecnologiaDTO deleteTecnologia(TecnologiaDTO tecnologiaDTO) {
+        try {
+            return tecnologiaService.deleteTecnologia(tecnologiaDTO);
+        } catch (SQLException e) {
+            System.err.println("Error LoginController en deleteLogin " + e.getMessage());
+            return null;
+        }
+    }
 }
